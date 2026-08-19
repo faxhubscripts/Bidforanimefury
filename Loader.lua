7a954e2187f7b8543c9852dbfea06ca2caae1c9e
faxhubscripts <josuecristian1612@gmail.com>
2026-08-19 00:42:24 -0400

    Enhance config check and update URL loading
    Updated the config check to print the enabled status and modified the URL loading logic.

--- Loader.lua
+++ Loader.lua
@@ -1,11 +1,14 @@
 local config = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Config.lua"
+    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Config.lua?t=" .. os.time()
 ))()
 
-if not config.Enabled then
+print("CONFIG ENABLED:", config.Enabled)
+
+if config.Enabled ~= true then
+    print("SCRIPT DISABLED")
     return
 end
 
 loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Bidforanimef"
+    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Bidforanimef?t=" .. os.time()
 ))()
