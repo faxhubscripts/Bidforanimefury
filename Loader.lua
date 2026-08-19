219e8af147358f4ef709441ce5c7b93f03e3351a
faxhubscripts <josuecristian1612@gmail.com>
2026-08-19 00:45:55 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,10 +2,7 @@ local config = loadstring(game:HttpGet(
     "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Config.lua?t=" .. os.time()
 ))()
 
-print("CONFIG ENABLED:", config.Enabled)
-
 if config.Enabled ~= true then
-    print("SCRIPT DISABLED")
     return
 end
 
