b64240b32a155d9dab2cfdd7ee796158b07a835a
faxhubscripts <josuecristian1612@gmail.com>
2026-08-19 00:58:56 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,17 +1,14 @@
 local config = loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Config.lua?t=" .. tostring(os.time())
+    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Config.lua"
 ))()
 
 print("ENABLED:", config.Enabled)
-print("TYPE:", typeof(config.Enabled))
 
 if config.Enabled ~= true then
     print("SCRIPT DISABLED")
     return
 end
 
-print("Loading script")
-
 loadstring(game:HttpGet(
-    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Bidforanimef?t=" .. tostring(os.time())
+    "https://raw.githubusercontent.com/faxhubscripts/Bidforanimefury/main/Bidforanimef"
 ))()
